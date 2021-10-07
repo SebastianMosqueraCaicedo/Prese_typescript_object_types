c69af752e46adfa69d0a10a85dc62d63838eb4b4
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-10-06 23:42:18 -0500

    Agrega codigo pertinente a readonly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("adios");
+interface Hogar{
+    readonly residente: {nombre: string; edad:number}
+}
+function visitaCumple (hogar:Hogar){
+    console.log("feliz cumple, "+ hogar.residente.nombre +"!");
+    hogar.residente.edad++;
+}
+
+// intenta SOBRE ESCRIBIR, error
+/* function evict(hogar:Hogar){
+    hogar.residente={
+        nombre:"tomas tomar",
+        edad: 21,
+    }
+} */
