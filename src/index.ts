f03ab0529abb958b5ff58276ffce13a0d1fa2fdd
Andrés NF23 <narvaezandresfelipe23@gmail.com>
2021-10-06 23:03:52 -0500

    Ejemplo OptionalProperties

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hola");
+interface Preguntas {
+
+    edad: number;
+    nombre: string;
+    sexo?: string;
+
+}
+function retorna (pregunta:Preguntas){
+
+    let edad = pregunta.edad;
+    let nombre = pregunta.nombre;
+    let sexo = pregunta.sexo;
+
+    console.log("Su nombre es " + nombre + ",su edad es " + edad)
+    if (sexo != undefined){
+        console.log(",su sexo es " + sexo)
+    }
+    let persona: Preguntas ={
+        edad: 12,
+        nombre: "Sebastian",
+        sexo: "m",
+    }
+    retorna(persona);
+}
+
